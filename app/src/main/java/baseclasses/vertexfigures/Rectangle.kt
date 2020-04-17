3355f67e9149b08b1a4455b2b4e68965a4b6bf61
mbnexttime <cgawrilow10@gmail.com>
2020-04-17 20:13:14 +0300

    represent rectangle by 2 points

--- app/src/main/java/baseclasses/vertexfigures/Rectangle.kt
+++ app/src/main/java/baseclasses/vertexfigures/Rectangle.kt
@@ -11,8 +11,7 @@ class Rectangle(
     text: MutableList<Char> = ArrayList(),
     texturePath: String = "",
     var leftDownCorner: Point = Point(),
-    var height: Int = 0,
-    var width: Int = 0
+    var rightUpCorner: Point = Point()
 ) : VertexFigure(text, texturePath)
 
 fun FigureInteractor.changeSize(rectangle: Rectangle, a: Stroke, b: Stroke) {
@@ -22,14 +21,15 @@ fun FigureInteractor.changeSize(rectangle: Rectangle, a: Stroke, b: Stroke) {
 fun FigureInteractor.moveByVector(rectangle: Rectangle, direction: Vector) {
     rectangle.leftDownCorner.x += direction.x
     rectangle.leftDownCorner.y += direction.y
+    rectangle.rightUpCorner.x += direction.x
+    rectangle.rightUpCorner.y = direction.y
 }
 
 fun VertexFigureNormalizer.normalizeRectangle(strokes: MutableList<Stroke>): Rectangle {
     val (maxX, maxY, minX, minY) = getStrokesRestrictions(strokes)
     return Rectangle(
-        leftDownCorner = Point(minX, minY, 0),
-        height = maxY - minY,
-        width = maxX - minX
+        leftDownCorner = Point(minX, minY),
+        rightUpCorner = Point(maxX, maxY)
     )
 }
 
