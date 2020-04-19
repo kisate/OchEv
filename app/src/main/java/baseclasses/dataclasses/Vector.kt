4f1e6a4381a0c2b01911d1353c30ead6bff6c0ef
mbnexttime <cgawrilow10@gmail.com>
2020-04-19 15:48:37 +0300

    debug fix

--- app/src/main/java/baseclasses/dataclasses/Vector.kt
+++ app/src/main/java/baseclasses/dataclasses/Vector.kt
@@ -10,6 +10,6 @@ class VectorInteractor {
     }
 
     fun getLength(vector: Vector): Float {
-        return sqrt((vector.x * vector.x + vector.y + vector.y).toFloat())
+        return sqrt((vector.x * vector.x + vector.y * vector.y).toFloat())
     }
 }
