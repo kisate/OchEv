a770b037731bb1f3fa11557ae6f7691ffc46e0c8
ainurbl <ainur19dota2@mail.ru>
2020-05-02 13:59:55 +0300

    Поменял константу ошибки касания редактирования.

--- app/src/main/java/com/example/ochev/baseclasses/vertexfigures/editors/VertexFigureMover.kt
+++ app/src/main/java/com/example/ochev/baseclasses/vertexfigures/editors/VertexFigureMover.kt
@@ -9,7 +9,7 @@ class VertexFigureMover(information: InformationForVertexEditor) {
     lateinit var previous: Point
 
     fun tryToStartMove(point: Point): Boolean {
-        return if (figure.getDistanceToPointOrZeroIfInside(point) <= 100f) {
+        return if (figure.getDistanceToPointOrZeroIfInside(point) <= 20f) {
             previous = point
             true
         } else {
