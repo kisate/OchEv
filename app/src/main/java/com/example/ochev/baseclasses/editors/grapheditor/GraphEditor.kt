acb1852bf414af23900c6111e813a36271ba47a6
mbnexttime <cgawrilow10@gmail.com>
2020-05-18 03:10:21 +0300

    last stroke

--- app/src/main/java/com/example/ochev/baseclasses/editors/grapheditor/GraphEditor.kt
+++ app/src/main/java/com/example/ochev/baseclasses/editors/grapheditor/GraphEditor.kt
@@ -3,10 +3,7 @@ package com.example.ochev.baseclasses.editors.grapheditor
 import com.example.ochev.baseclasses.EdgeFigure
 import com.example.ochev.baseclasses.FigureNormalizer
 import com.example.ochev.baseclasses.VertexFigure
-import com.example.ochev.baseclasses.dataclasses.Graph
-import com.example.ochev.baseclasses.dataclasses.InformationForNormalizer
-import com.example.ochev.baseclasses.dataclasses.InformationForVertexEditor
-import com.example.ochev.baseclasses.dataclasses.Point
+import com.example.ochev.baseclasses.dataclasses.*
 import com.example.ochev.baseclasses.edgefigures.Line
 import com.example.ochev.baseclasses.editors.vertexeditor.VertexFigureEditor
 
@@ -64,25 +61,49 @@ class GraphEditor(
     fun replaceVertex(old: VertexFigure, new: VertexFigure) {
         // redirecting edges
         val newGraph = Graph()
-
-        graph.figures.edges.forEach {
-            if (it.first.beginFigure == old) {
-                when (it.first) {
-                    is Line -> newGraph.figures.addEdge(Line(new, it.first.endFigure), it.second)
-                }
-            } else if (it.first.endFigure == old) {
-                when (it.first) {
-                    is Line -> newGraph.figures.addEdge(Line(it.first.endFigure, new), it.second)
-                }
-            } else {
-                newGraph.figures.edges.add(it)
-            }
+        val linker = getLinker {
+            if (it == old) new
+            else it
         }
+
+        newGraph.figures.edges += reconnectEdges(linker)
 
         graph.figures.vertexes.forEach {
             if (it.first == old) newGraph.figures.addVertex(new, it.second)
             else newGraph.figures.vertexes.add(it)
         }
+
+        graph = newGraph
+    }
+
+    fun getLinker(changeFun: (VertexFigure) -> VertexFigure): HashMap<VertexFigure, VertexFigure> {
+        val linker: HashMap<VertexFigure, VertexFigure> = HashMap()
+        graph.figures.vertexes.forEach { linker[it.first] = changeFun(it.first) }
+        return linker
+    }
+
+    fun reconnectEdges(linker: HashMap<VertexFigure, VertexFigure>): MutableList<Pair<EdgeFigure, Int>> {
+        val result: MutableList<Pair<EdgeFigure, Int>> = ArrayList()
+        graph.figures.edges.forEach {
+            when (it.first) {
+                is Line -> result.add(
+                    Pair(
+                        Line(linker[it.first.beginFigure]!!, linker[it.first.endFigure]!!),
+                        it.second
+                    )
+                )
+            }
+        }
+        return result
+    }
+
+    fun moveGraphByVector(vector: Vector) {
+        val newGraph = Graph()
+        val linker = getLinker { it.movedByVector(vector) }
+        val newEdges = reconnectEdges(linker)
+
+        graph.figures.vertexes.forEach { newGraph.figures.addVertex(linker[it.first]!!, it.second) }
+        newGraph.figures.edges += newEdges
 
         graph = newGraph
     }
