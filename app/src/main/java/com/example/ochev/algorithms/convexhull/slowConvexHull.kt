ff0d06b97e3114412348555c5172d4581e349500
ainurbl <ainur19dota2@mail.ru>
2020-05-19 21:51:36 +0300

    Восстановил файл.

---
+++ app/src/main/java/com/example/ochev/algorithms/convexhull/slowConvexHull.kt
@@ -0,0 +1,36 @@
+package com.example.ochev.algorithms.convexhull
+
+import com.example.ochev.baseclasses.dataclasses.Point
+import com.example.ochev.baseclasses.dataclasses.Stroke
+import com.example.ochev.baseclasses.dataclasses.Stroke.Companion.joinListOfStrokes
+import com.example.ochev.baseclasses.dataclasses.Vector
+
+
+fun ConvexHullMaker.slowConvexHull(strokes: MutableList<Stroke>): Stroke {
+    val uniquePoints = joinListOfStrokes(strokes)
+    val result = Stroke()
+
+    val usedPoints: HashMap<Point, Boolean> = HashMap()
+    var currentPoint = uniquePoints.points.maxBy { it.y }!!
+
+    class ComparatorByPolarAngle(val mainPoint: Point) : Comparator<Point> {
+        override fun compare(A: Point?, B: Point?): Int {
+            if (A == null && B == null) return 0
+            if (A == null || A == mainPoint) return -1
+            if (B == null || B == mainPoint) return 1
+            val vectorAB = Vector(mainPoint, A)
+            val vectorAC = Vector(mainPoint, B)
+            return vectorAB.x * vectorAC.y - vectorAB.y * vectorAC.x
+        }
+    }
+
+    while (!usedPoints.containsKey(currentPoint)) {
+        result.addPoint(currentPoint)
+        usedPoints[currentPoint] = true
+
+        uniquePoints.points.sortWith(ComparatorByPolarAngle(currentPoint))
+        currentPoint = uniquePoints.points.last()
+    }
+
+    return result
+}
