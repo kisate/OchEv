718b7c570e7513916da8716e8af9c43f89874c8f
kisate <dhkas@mail.ru>
2020-05-20 17:59:03 +0300

    Zoom + scroll(partially)

--- app/src/main/java/com/example/ochev/viewclasses/eventhandlers/ScrollAndZoomEventHandler.kt
+++ app/src/main/java/com/example/ochev/viewclasses/eventhandlers/ScrollAndZoomEventHandler.kt
@@ -7,39 +7,69 @@ import com.example.ochev.baseclasses.dataclasses.Vector
 import com.example.ochev.ml.Classifier
 import com.example.ochev.viewclasses.GraphDrawer
 import com.example.ochev.viewclasses.StrokeDrawer
-import com.example.ochev.viewclasses.buttonshandler.ButtonsHandler
+import kotlin.math.sqrt
 
 class ScrollAndZoomEventHandler(
     strokeDrawer: StrokeDrawer,
-    drawGraphView: GraphDrawer,
+    private val drawGraphView: GraphDrawer,
     classifier: Classifier
 ) : GestureEventHandler(strokeDrawer, drawGraphView, classifier) {
 
     private var lastCenter: Point? = null
     private var lastDistance: Float? = null
+    private var firstPointerId: Int? = null
+    private var secondPointerId: Int? = null
 
     override fun handle(gesture: Gesture, event: MotionEvent){
 
-        when (gesture.state) {
-            GestureState.START -> {
-                lastCenter = Point(event)
-                lastDistance = calcDistance(event)
-
-            }
-        }
-
-        if (gesture.state == GestureState.START)
-        {
-
-        }
-
         if (event.pointerCount == 2)
         {
-            Log.i("Scrolling", event.getPointerId(0).toString())
-            Log.i("Scrolling", "${event.getX(0)} ${event.getY(0)}")
-            Log.i("Scrolling", "${event.getX(1)} ${event.getY(1)}")
-            Log.i("Scrolling", "${event.x} ${event.y}")
-            Log.i("Scrolling", "${Vector(calcCenter(event), lastCenter!!)}")
+            when (gesture.state) {
+                GestureState.NONE -> {
+
+                }
+                GestureState.START -> {
+                    lastCenter = calcCenter(event)
+                    lastDistance = calcDistance(event)
+                    firstPointerId = event.getPointerId(0)
+                    secondPointerId = event.getPointerId(1)
+                }
+                GestureState.IN_PROGRESS -> {
+                    Log.i("Scrolling", event.getPointerId(0).toString())
+                    Log.i("Scrolling", "${event.getX(0)} ${event.getY(0)}")
+                    Log.i("Scrolling", "${event.getX(1)} ${event.getY(1)}")
+                    Log.i("Scrolling", "${event.x} ${event.y}")
+                    Log.i("Scrolling", "${Vector(calcCenter(event), lastCenter!!)}")
+                    Log.i("Scrolling", "${calcDistance(event)}")
+                    Log.i("Scrolling", "$firstPointerId $secondPointerId")
+
+//                    drawGraphView.graphView.graphEditor.moveGraphByVector(Vector(lastCenter!!, calcCenter(event)))
+
+                    val factor = calcDistance(event)/lastDistance!!
+
+                    Log.d("Scrolling", factor.toString())
+
+                    if (factor > ZOOM_THRESHOLD)
+                    {
+                        drawGraphView.graphView.graphEditor.zoomByPointAndFactor(calcCenter(event), factor)
+                    }
+                    if (factor < 1/ZOOM_THRESHOLD)
+                    {
+                        drawGraphView.graphView.graphEditor.zoomByPointAndFactor(calcCenter(event), -factor)
+                    }
+
+                    drawGraphView.graphView.invalidate()
+
+                    lastDistance = calcDistance(event)
+                    lastCenter = calcCenter(event)
+                }
+                GestureState.END -> {
+                    lastCenter = null
+                    lastDistance = null
+                    firstPointerId = null
+                    secondPointerId = null
+                }
+            }
         }
     }
 
@@ -54,5 +84,9 @@ class ScrollAndZoomEventHandler(
     private fun calcDistance(event: MotionEvent): Float {
 
         return Point(event.getX(0).toInt(), event.getY(0).toInt()).getDistanceToPoint(Point(event.getX(1).toInt(), event.getY(1).toInt()))
+    }
+
+    companion object {
+        private const val ZOOM_THRESHOLD = 1.01f
     }
 }
