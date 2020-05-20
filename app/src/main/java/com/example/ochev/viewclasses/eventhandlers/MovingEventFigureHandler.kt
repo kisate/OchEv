360f57131688f72d553df5d18062e536109c691c
ainurbl <ainur19dota2@mail.ru>
2020-05-20 23:11:19 +0300

    Добавлены ресайз и перемещение.

--- app/src/main/java/com/example/ochev/viewclasses/eventhandlers/MovingEventFigureHandler.kt
+++ app/src/main/java/com/example/ochev/viewclasses/eventhandlers/MovingEventFigureHandler.kt
@@ -2,6 +2,7 @@ package com.example.ochev.viewclasses.eventhandlers
 
 import android.util.Log
 import android.view.MotionEvent
+import com.example.ochev.baseclasses.dataclasses.Point
 import com.example.ochev.baseclasses.editors.vertexeditor.VertexFigureEditor
 import com.example.ochev.ml.Classifier
 import com.example.ochev.viewclasses.GraphDrawer
@@ -9,15 +10,28 @@ import com.example.ochev.viewclasses.StrokeDrawer
 
 class MovingEventFigureHandler(
     strokeDrawer: StrokeDrawer,
-    drawGraphView: GraphDrawer,
+    private val drawGraphView: GraphDrawer,
     classifier: Classifier,
-    vertexFigureEditor: VertexFigureEditor
+    private val vertexFigureEditor: VertexFigureEditor
 ) : GestureEventHandler(strokeDrawer, drawGraphView, classifier) {
 
     override fun handle(gesture: Gesture, event: MotionEvent) {
+        val point = event.let{ Point(it) }
+        when (gesture.state) {
+            GestureState.START -> {
+                vertexFigureEditor.mover.moveBegins(point)
+            }
+            GestureState.IN_PROGRESS -> {
+                vertexFigureEditor.mover.nextPoint(point)
+            }
+            GestureState.END -> {
+                vertexFigureEditor.mover.nextPoint(point)
+            }
+            else -> {
 
-
-
+            }
+        }
+        drawGraphView.graphView.invalidate()
     }
 
 }
--- app/src/main/java/com/example/ochev/viewclasses/eventhandlers/ShapingEventHandler.kt
+++ app/src/main/java/com/example/ochev/viewclasses/eventhandlers/ShapingEventHandler.kt
@@ -2,6 +2,7 @@ package com.example.ochev.viewclasses.eventhandlers
 
 import android.util.Log
 import android.view.MotionEvent
+import com.example.ochev.baseclasses.dataclasses.Point
 import com.example.ochev.baseclasses.editors.vertexeditor.VertexFigureEditor
 import com.example.ochev.ml.Classifier
 import com.example.ochev.viewclasses.GraphDrawer
@@ -9,11 +10,27 @@ import com.example.ochev.viewclasses.StrokeDrawer
 
 class ShapingEventHandler(
     strokeDrawer: StrokeDrawer,
-    drawGraphView: GraphDrawer,
+    private val drawGraphView: GraphDrawer,
     classifier: Classifier,
-    vertexFigureEditor: VertexFigureEditor
+    private val vertexFigureEditor: VertexFigureEditor
 ) : GestureEventHandler(strokeDrawer, drawGraphView, classifier) {
+
     override fun handle(gesture: Gesture, event: MotionEvent) {
-        Log.d("Gestures", "Shaping")
+        val point = event.let{ Point(it) }
+        when (gesture.state) {
+            GestureState.START -> {
+                vertexFigureEditor.shaper.shapingBegins(point)
+            }
+            GestureState.IN_PROGRESS -> {
+                vertexFigureEditor.shaper.nextPoint(point)
+            }
+            GestureState.END -> {
+                vertexFigureEditor.shaper.nextPoint(point)
+            }
+            else -> {
+
+            }
+        }
+        drawGraphView.graphView.invalidate()
     }
 }
