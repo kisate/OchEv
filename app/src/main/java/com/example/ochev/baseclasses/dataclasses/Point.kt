4c320b1a275c793f24256f6772d801c853c5e5cd
ainurbl <ainur19dota2@mail.ru>
2020-05-22 20:43:25 +0300

    Добавлено пересечение отрезков.

--- app/src/main/java/com/example/ochev/baseclasses/dataclasses/Point.kt
+++ app/src/main/java/com/example/ochev/baseclasses/dataclasses/Point.kt
@@ -1,6 +1,7 @@
 package com.example.ochev.baseclasses.dataclasses
 
 import android.view.MotionEvent
+import kotlin.math.abs
 import kotlin.math.min
 import kotlin.math.sqrt
 
@@ -47,5 +48,27 @@ data class Point(val x: Float = 0f, val y: Float = 0f) {
             getDistanceToPoint(linePointA),
             getDistanceToPoint(linePointB)
         )
+    }
+
+    companion object{
+        fun intersectTwoSegments(p1: Point, p2: Point, p3: Point, p4: Point): Point? {
+            val v1 = Vector(p1, p2)
+            val v2 = Vector(p3, p4)
+            if (abs(v1.vectorProduct(v2)) <= 0.000001f){
+                return null;
+            }
+
+            val b = (p1.x*v1.y + p3.x*v1.x - p1.y*v1.x - p3.x*v1.y) / (v2.x*v1.y - v2.y*v1.x)
+            var a = 0f
+            if (abs(v1.x) <= 0.000001f)a = (b*v2.x + p3.x - p1.x)/v1.x
+            else a = (b*v2.y + p3.y - p1.y)/v1.y
+
+            val interestingPoint = Point(p1.x + v1.x * a, p1.y + v1.y * a)
+
+            if (abs(interestingPoint.getDistanceToPoint(p1) + interestingPoint.getDistanceToPoint(p2) - p1.getDistanceToPoint(p2)) <= 0.000001f ) {
+                return interestingPoint
+            }
+            return null
+        }
     }
 }
