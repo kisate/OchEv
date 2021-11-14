a51e9ed404c6a78940273b24d91d420ad24810f9
ainurbl <ainur19dota2@mail.ru>
2021-11-14 12:05:20 +0300

    add support of intersection of two suggest lines

--- app/src/main/java/com/example/ochev/baseclasses/editors/vertexeditor/MoverHelper.kt
+++ app/src/main/java/com/example/ochev/baseclasses/editors/vertexeditor/MoverHelper.kt
@@ -7,7 +7,7 @@ import com.example.ochev.baseclasses.dataclasses.Vector
 class MoverHelper(
     val editor: VertexFigureEditor
 ) {
-    fun getPossibleLines(): LineSegment? {
+    fun correctingSegments(): List<LineSegment> {
         val result: MutableList<LineSegment> = ArrayList()
         editor.graphEditor.allVertexes.forEach {
             if (it.figure != editor.currentFigureState)
@@ -16,23 +16,35 @@ class MoverHelper(
         result.removeIf {
             editor.currentFigureState.center.getDistanceToLineSegment(it) > 50f
         }
-        return result.minByOrNull { editor.currentFigureState.center.getDistanceToLineSegment(it) }
-    }
-
-    fun correctingSegment(): LineSegment? {
-        return getPossibleLines()
+        for (i in result.indices) {
+            for (j in i + 1 until result.size) {
+                if (result[i].toVector().scalarProduct(result[j].toVector()) <= 1e-5) {
+                    return listOf(result[i], result[j])
+                }
+            }
+        }
+        return listOfNotNull(result.minByOrNull {
+            editor.currentFigureState.center.getDistanceToLineSegment(
+                it
+            )
+        })
     }
 
     fun getCorrector(): Vector? {
-        val bestLine = correctingSegment()
-        if (bestLine != null) {
+        val bestLine = correctingSegments()
+        var resVector: Vector? = null
+        val cur = editor.currentFigureState.center
+        if (bestLine.size == 1) {
             val segmentToLine = Point.getOptimalSegment(
-                bestLine,
-                editor.currentFigureState.center
+                bestLine[0],
+                cur
             )
-            val direction = Vector(segmentToLine.A, segmentToLine.B)
-            return direction
-       }
-        return null
+            resVector = Vector(segmentToLine.A, segmentToLine.B)
+        }
+        if (bestLine.size == 2) {
+            val pt: Point? = Point.intersectTwoSegments(bestLine[0], bestLine[1])
+            resVector = Vector(pt!!.x - cur.x, pt.y - cur.y)
+        }
+        return resVector
     }
 }
